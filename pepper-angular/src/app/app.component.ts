414fc1fb9323c3f8959021ebf93abc3f42a1a81f
Alec Tecson <alec.tecson@nipa.ph>
2020-05-27 15:39:38 +0800

    Working with the Current User

--- pepper-angular/src/app/app.component.ts
+++ pepper-angular/src/app/app.component.ts
@@ -8,20 +8,34 @@ import { auth } from 'firebase/app';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  
+  displayName;
+  photoURL;
+
   constructor(private auth: AngularFireAuth) {
 
   }
 
   ngOnInit() {
-    
+    this.auth.authState.subscribe(authState => {
+      if (!authState){
+        this.displayName = null;
+        this.photoURL = null;
+        return;
+      }
+
+      this.displayName = authState.displayName
+      this.photoURL = authState.photoURL; 
+    })
   }  
 
   login() {
     let provider = new auth.FacebookAuthProvider();
+    provider.setCustomParameters({
+      'display': 'popup'
+    });
     this.auth.signInWithPopup(provider).then(authState => {
       console.log("AFTER LOGIN", authState);
-    })
+    }).catch(err => console.log(err))
   }
 
   logout() {
