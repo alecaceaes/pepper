490cf4c76d5655d0bf1fab24326b6c5d7cf22195
Alec Tecson <alec.tecson@nipa.ph>
2020-05-27 15:13:31 +0800

    Joining

--- pepper-angular/src/app/app.component.ts
+++ pepper-angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,40 +10,26 @@ import { Observable } from 'rxjs/internal/Observable';
 })
 export class AppComponent implements OnInit {
   title = 'pepper-angular';
-  subscription: AngularFireList<any>;
-  restaurant;	  cuisines: Observable<any[]>;
+  subscription: AngularFireList<any>; 
+  cuisines: Observable<any[]>;
+  restaurants: Observable<any[]>;
 
   constructor(private af: AngularFireDatabase) {
 
   }
 
   ngOnInit() {
-    this.subscription = this.af.list('cuisines');
-    this.cuisines = this.subscription.snapshotChanges();
+    this.cuisines = this.af.list('cuisines').snapshotChanges();
+    this.restaurants = this.af.list('restaurants').valueChanges().pipe(
+      map(restaurants => {
+        console.log("BEFORE MAP", restaurants);
+        restaurants.map(restaurant => {
+          restaurant['cuisineType'] = this.af.object('cuisines/' + restaurant['cuisine']).valueChanges();
+        })
+        console.log("AFTER MAP", restaurants);
+        return restaurants;
+      })
+    );
   }	  
-
-  add() {
-    this.subscription.push({
-      name: 'Asian',
-      details: {
-        description : '...'
-      }
-    });
-  }	  
-
-  update() {
-    // this.af.object('restaurant').update({
-    // this.af.object('restaurant').set({
-    //   name: 'New Name',
-    //   rating: 5
-    // })
-    // this.af.object('favorites/1/10').set(true);
-    this.af.object('favorites/1/10').set(null);
-  }
-
-  remove() {
-    this.af.object('restaurant').remove()
-      .then(x => console.log("SUCCESS"))
-      .catch(error => console.log("ERROR", error));
-  }
+  
 }
