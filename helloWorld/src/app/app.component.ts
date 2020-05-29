8c313886d55d4c07e9c6099a4660d27a7607f0ea
Alec Tecson <alec.tecson@nipa.ph>
2020-05-29 14:55:05 +0800

    Platform

--- helloWorld/src/app/app.component.ts
+++ helloWorld/src/app/app.component.ts
@@ -20,6 +20,9 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
+      if (!this.platform.is('cordova'))
+        return;
+        
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
