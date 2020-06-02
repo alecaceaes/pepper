0e85b72f4abbf1dc10c35b14c89ee3015317c165
Alec Tecson <alec.tecson@nipa.ph>
2020-06-02 18:00:53 +0800

    Angular Universal and Server-side Pre-rendering

--- apple/Startup.cs
+++ apple/Startup.cs
@@ -31,6 +31,8 @@ namespace apple
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -64,6 +66,20 @@ namespace apple
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
                 spa.Options.SourcePath = "ClientApp";
+                spa.UseSpaPrerendering(options =>  {
+                    // This is the path where angular generates the server build result
+                    // This path would be different when using React or Vue
+                    options.BootModulePath = $"{spa.Options.SourcePath}/dist/ClientApp/server/main.js";
+
+                    // During development, let angular run the build:ssr command on each build.
+                    // Check the package.json to see what this command does.
+                    // When deploying your application on a production server, the ng build:ssr command will be executed to generate the server build.
+                    options.BootModuleBuilder = env.IsDevelopment()
+                        ? new AngularCliBuilder(npmScript: "build:ssr")
+                        : null;
+
+                    options.ExcludeUrls = new[] { "/sockjs-node" };
+                });
 
                 if (env.IsDevelopment())
                 {
