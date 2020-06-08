ccd5494986a56de623dd6bff24ddf53f4997e202
Alec Tecson <alec.tecson@nipa.ph>
2020-06-08 17:04:10 +0800

    Refactoring Fat Case Statements

--- redux-demo/src/app/store.ts
+++ redux-demo/src/app/store.ts
@@ -11,70 +11,92 @@ export const INITIAL_STATE: IAppState = {
   lastUpdate: null
 };
 
-export function rootReducer(state: IAppState, action): IAppState {
-  switch (action.type) {
-    case ADD_TODO:
-      let newTodo = { id: state.todos.length + 1, title: action.title };
+function addTodo(state, action) {
+    let newTodo = { id: state.todos.length + 1, title: action.title };
 
-      return tassign(state, {
-        // Instead of the push() method, we use the concat() method because the former mutates
-        // the original array, whereas the latter returns a new array. 
-        todos: state.todos.concat(newTodo),
-        lastUpdate: new Date()
-      });
+    return tassign(state, {
+    // Instead of the push() method, we use the concat() method because the former mutates
+    // the original array, whereas the latter returns a new array. 
+    todos: state.todos.concat(newTodo),
+    lastUpdate: new Date()
+    });
+}
 
-    case TOGGLE_TODO:
-      // When modifying an item in an array, we should create a new array, and copy 
-      // all other item from the source array (except the item to be modified). At the same time
-      // we should create a copy of the item to be modified and apply the mutations using tassing.
+function toggleTodo(state, action) {
+    // When modifying an item in an array, we should create a new array, and copy 
+    // all other item from the source array (except the item to be modified). At the same time
+    // we should create a copy of the item to be modified and apply the mutations using tassing.
 
-      // So, first we need to find the item to be modified. Here, we are finding it by it's id. 
-      let todo = state.todos.find(t => t.id === action.id);
+    // So, first we need to find the item to be modified. Here, we are finding it by it's id. 
+    let todo = state.todos.find(t => t.id === action.id);
 
-      // Now, we need to find the position of this item in the array. 
-      let index = state.todos.indexOf(todo);
+    // Now, we need to find the position of this item in the array. 
+    let index = state.todos.indexOf(todo);
 
-      return tassign(state, {
+    return tassign(state, {
         todos: [
-          // Using the slice() method, we can slice an array. This method does not mutate the 
-          // original array, and returns a new array. So here, we're getting all the items from 
-          // the beginning to the index of the item we're going to modiy. 
-          //
-          // We use the spread operator (...) to enumerate an array. This is a clean way to 
-          // concat two arrays. Instead of
-          //
-          // var newArray = [];
-          // newArray.concat(sourceArray1).concat(sourceArray2);
-          //
-          // We can write:
-          //
-          // var newArray = [...sourceArray1, ...sourceArray2];
-          ...state.todos.slice(0, index),
+            // Using the slice() method, we can slice an array. This method does not mutate the 
+            // original array, and returns a new array. So here, we're getting all the items from 
+            // the beginning to the index of the item we're going to modiy. 
+            //
+            // We use the spread operator (...) to enumerate an array. This is a clean way to 
+            // concat two arrays. Instead of
+            //
+            // var newArray = [];
+            // newArray.concat(sourceArray1).concat(sourceArray2);
+            //
+            // We can write:
+            //
+            // var newArray = [...sourceArray1, ...sourceArray2];
+            ...state.todos.slice(0, index),
 
-          // So, we have copied all the items before the item to be modified. Now, we take a copy
-          // of this item and apply the mutation (isCompleted).
-          tassign(todo, { isCompleted: !todo.isCompleted }),
+            // So, we have copied all the items before the item to be modified. Now, we take a copy
+            // of this item and apply the mutation (isCompleted).
+            tassign(todo, { isCompleted: !todo.isCompleted }),
 
-          // Now, we need to copy all the items after this item. Again, we use the slice() method
-          // to get all the items following that item, and use the spread operator to enumerate 
-          // them and put them in our target array.
-          ...state.todos.slice(index + 1),
+            // Now, we need to copy all the items after this item. Again, we use the slice() method
+            // to get all the items following that item, and use the spread operator to enumerate 
+            // them and put them in our target array.
+            ...state.todos.slice(index + 1),
         ],
         lastUpdate: new Date()
-      });
+    });
+}
 
-    case REMOVE_TODO:
-      return tassign(state, {
+function removeTodo(state, action) {
+    return tassign(state, {
         todos: state.todos.filter(t => t.id !== action.id),
         lastUpdate: new Date()
-      });
+    });
+}
 
-    case CLEAR_TODOS:
-     return tassign(state, {
+function clearTodos(state, action) {
+    return tassign(state, {
         todos: [],
         lastUpdate: new Date()
-      });
-  }
+    });
+}
 
-  return state;
+class TodoActions {
+    constructor(private state, private action) {
+
+    }
+
+    addTodo() {
+
+    }
+}
+
+export function rootReducer(state: IAppState, action): IAppState {
+    // let actions = new TodoActions(state, action);
+
+    switch (action.type) {
+        case ADD_TODO: return addTodo(state, action);
+        // case ADD_TODO: return actions.addTodo();
+        case TOGGLE_TODO: return toggleTodo(state, action);
+        case REMOVE_TODO: return removeTodo(state, action);
+        case CLEAR_TODOS: return clearTodos(state, action);
+    }
+
+    return state;
 }
