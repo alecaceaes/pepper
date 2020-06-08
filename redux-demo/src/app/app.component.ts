f2ef89199b842f1b688ea789f8d1f318896e8d4d
Alec Tecson <alec.tecson@nipa.ph>
2020-06-08 13:40:45 +0800

    Using Imuutable.js

--- redux-demo/src/app/app.component.ts
+++ redux-demo/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { INCREMENT } from './actions';
 import { NgRedux, select } from '@angular-redux/store';
 import { Component } from '@angular/core';
 import { IAppState } from './store';
+import { Map } from 'immutable';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,11 @@ import { IAppState } from './store';
 export class AppComponent {
   title = 'redux-demo';
   // counter = 0;
-  @select('counter') count;
+  @select(s => s.get('counter')) count;
   // @select(['messaging', 'newMessages']) newMessages;
   // @select((s: IAppState) => s.messaging.newMessages) newMessageCount;
 
-  constructor(private ngRedux: NgRedux<IAppState>) {
+  constructor(private ngRedux: NgRedux<Map<string, any>>) {
     // ngRedux.subscribe(() => {
     //   let store = ngRedux.getState();
     //   this.counter = store.counter;
--- redux-demo/src/app/app.module.ts
+++ redux-demo/src/app/app.module.ts
@@ -1,7 +1,8 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgRedux, NgReduxModule } from '@angular-redux/store'
+import { NgRedux, NgReduxModule } from '@angular-redux/store';
+import { fromJS, Map } from 'immutable';
 
 import { AppComponent } from './app.component';
 import { IAppState, rootReducer, INITIAL_STATE } from './store';
@@ -17,9 +18,8 @@ import { IAppState, rootReducer, INITIAL_STATE } from './store';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { 
-  constructor(ngRedux: NgRedux<IAppState>) {
-    ngRedux.configureStore(rootReducer, INITIAL_STATE, []);
+export class AppModule {
+  constructor(ngRedux: NgRedux<Map<string, any>>) {
+    ngRedux.configureStore(rootReducer, fromJS(INITIAL_STATE), []);
   }
 }
- 
